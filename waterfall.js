a4a9dd7feb31915cb45dcd5a0607d852809fa40d
cage <517020375@qq.com>
2012-06-29 13:49:26 +0800

    ...

--- waterfall.js
+++ waterfall.js
@@ -1,222 +1,268 @@
-var data="";
-data += "				<li class=\"justify_item\"><a href=\"#\"><img src=\"img\/1.jpg\" alt=\"\" ><span>00<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/2.jpg\" alt=\"\" ><span>01<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/3.jpg\" alt=\"\" ><span>02<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/4.jpg\" alt=\"\" ><span>03<\/span><\/a><\/li>";
-data += "				<li class=\"justify_item\"><a href=\"#\"><img src=\"http:\/\/image9.9158.com\/44\/31\/519439339/D12C3E2A1822DDD416B26C45B79ABADF.jpg\" alt=\"\" ><span>00<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/5.jpg\" alt=\"\" ><span>04<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/6.jpg\" alt=\"\" ><span>05<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/7.jpg\" alt=\"\" ><span>06<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/8.jpg\" alt=\"\" ><span>07<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/9.jpg\" alt=\"\" ><span>08<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/10.jpg\" alt=\"\" ><span>09<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/11.jpg\" alt=\"\" ><span>10<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/12.jpg\" alt=\"\" ><span>11<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/13.jpg\" alt=\"\" ><span>12<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/14.jpg\" alt=\"\" ><span>13<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/15.jpg\" alt=\"\" ><span>14<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/16.jpg\" alt=\"\" ><span>15<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/17.jpg\" alt=\"\" ><span>16<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/18.jpg\" alt=\"\" ><span>17<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/19.jpg\" alt=\"\" ><span>18<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/20.jpg\" alt=\"\" ><span>19<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/21.jpg\" alt=\"\" ><span>20<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/22.jpg\" alt=\"\" ><span>21<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/23.jpg\" alt=\"\" ><span>22<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/24.jpg\" alt=\"\" ><span>23<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/25.jpg\" alt=\"\" ><span>24<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/26.jpg\" alt=\"\" ><span>25<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/27.jpg\" alt=\"\" ><span>26<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/28.jpg\" alt=\"\" ><span>27<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/29.jpg\" alt=\"\" ><span>28<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/30.jpg\" alt=\"\" ><span>29<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/31.jpg\" alt=\"\" ><span>30<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/32.jpg\" alt=\"\" ><span>31<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/33.jpg\" alt=\"\" ><span>32<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/34.jpg\" alt=\"\" ><span>33<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/35.jpg\" alt=\"\" ><span>34<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/36.jpg\" alt=\"\" ><span>35<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/37.jpg\" alt=\"\" ><span>36<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/38.jpg\" alt=\"\" ><span>37<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/39.jpg\" alt=\"\" ><span>38<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/40.jpg\" alt=\"\" ><span>39<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/41.jpg\" alt=\"\" ><span>40<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/42.jpg\" alt=\"\" ><span>41<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/43.jpg\" alt=\"\" ><span>42<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/44.jpg\" alt=\"\" ><span>43<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/45.jpg\" alt=\"\" ><span>44<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/46.jpg\" alt=\"\" ><span>45<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/47.jpg\" alt=\"\" ><span>46<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/48.jpg\" alt=\"\" ><span>47<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/49.jpg\" alt=\"\" ><span>48<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/50.jpg\" alt=\"\" ><span>49<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/51.jpg\" alt=\"\" ><span>50<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/52.jpg\" alt=\"\" ><span>51<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/53.jpg\" alt=\"\" ><span>52<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/54.jpg\" alt=\"\" ><span>53<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/55.jpg\" alt=\"\" ><span>54<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/56.jpg\" alt=\"\" ><span>55<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/57.jpg\" alt=\"\" ><span>56<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/58.jpg\" alt=\"\" ><span>57<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/59.jpg\" alt=\"\" ><span>58<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/60.jpg\" alt=\"\" ><span>59<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/61.jpg\" alt=\"\" ><span>60<\/span><\/a><\/li>";
-data += "			   <li class=\"justify_item\"><a href=\"#\"><img src=\"img\/62.jpg\" alt=\"\" ><span>61<\/span><\/a><\/li>";
-
-function children(ele){
-	var temp = ele.childNodes;
-	var len = temp.length;
-	var item = [];
-	while(len--){
-		temp[len].nodeType === 1 && item.unshift(temp[len]);
+(function(){
+	var ajax = function(options){
+		return new ajax.init(options);
 	}
-	return item;
-}
 
-function imgReady(imgs,fn,context){
-	var len = imgs.length;
-	var length = len;
-	for(var i = 0; i < len; i++){
-		if(typeof imgs[i].onreadystatechange !== "undefined"){
-			imgs[i].onreadystatechange = function(){
-				if(this.readyState === "complete"){
-					length--;
-					document.title = "还剩 " + length + " 张图片!";
-					!length && fn.call(context);
+	ajax.init = function(options){
+		this.request = this.create();
+		this.data = this.getDetails(options);
+	}
+
+	ajax.init.prototype = {
+		create : function(){
+			var request = null;
+			try{
+				request = new XMLHttpRequest();
+			}catch(MS){
+				try{
+					request = new ActiveXObject("Msxml2.XMLHTTP");
+				}catch(otherMs){
+					try{
+						request = new ActiveXObject("Microsoft.XMLHTTP");
+					}catch(failed){
+						return request;
+					}
 				}
 			}
-		}else{
-			imgs[i].onload = function(){
-				length--;
-				document.title = "还剩 " + length + " 张图片!";
-				!length && fn.call(context);
-			}
-		}
-	}
-}
+			return request;
+		},
 
-var waterfall = function(options){
-	var _this = this;
-	for(var item in options){
-		if(!options.hasOwnProperty(item)){continue;}
-		this[item] = options[item];
-	}
-	this.colnum = Math.floor(document.documentElement.clientWidth / this.w);
-	this.filled = [];
-
-	this.appendFilled = function(obj){
-		var oFragment = document.createDocumentFragment();
-		var len = this.filled.length;
-		for(var i = 0; i < this.colnum - len; i++){
-			var oFilled = document.createElement("li");
-			oFilled.className = this.itemClassName;
-			oFilled.style.cssText = "height:0;overflow:hidden;visibility:hidden;padding-top:0;padding-bottom:0;";
-			if(document.querySelector){
-				var oNbsp = document.createTextNode("\n");
-				oFragment.appendChild(oNbsp);
+		getDetails : function(options){
+			var request = this.request;
+			var url = this.url = options['url'];
+			var success = this.success = options['success'];
+			var complete = this.complete = options['complete'];
+			var error = this.error = options['error'];
+			var start = this.start = options['start'];
+			var stop = this.stop = options['stop'];
+			if(request === null){
+				alert("Unable to create request");
+				return;
 			}
-			oFragment.appendChild(oFilled);
-			this.filled.push(oFilled);
+			request.onreadystatechange = function(){
+				(function(){
+					if(this.readyState === 4){
+						if(this.status === 200){
+							success(this.responseText);
+						}else if(this.status !== 200){
+							error("Page is Not Found!");
+						}
+					}
+				}).call(request);
+			};
+			request.open("GET",url,true);
+			request.send(null);
 		}
-		obj.appendChild(oFragment);
-	}
-	this.removeFilled = function(){
-		var len = this.filled.length;
-		for(var i = 0; i < len; i++){
-			var oFilled = this.filled[i];
-			oFilled.parentNode.removeChild(oFilled);
-		}
-		this.filled = [];
+
 	};
 
-	this.context = document.getElementById(this.id);
-	this.maxH = 0;
-	this.cols = [];
+	var waterfall = function(options){
+		return new waterfall.init(options);
+	};
 
-	this.load = function(eles,rsize){
-		var temp = document.createElement("ul");
-		var old = children(this.context);
-		temp.style.cssText = "height:0;visibility:hidden;overflow:hidden";
-		if(typeof eles === "string"){
-			temp.innerHTML = eles;
-		}else{
-			for(var i = 0,len = eles; i < len; i++){
-				temp.appendChild(eles[i]);
-			}
+	waterfall.init = function(options){
+		var _this = this;
+		this.ncolnum = 2;
+		this.length = 0;
+		this.maxH = 0;
+		this.finished = false;
+		this.cols = [];
+		this.filled = [];
+		for(var item in options){
+			if(!options.hasOwnProperty(item)){continue;}
+			this[item] = options[item];
 		}
-		eles = old.concat(children(temp));
-		this.context.appendChild(temp);
+		this.box = document.getElementById(this.id);
+		this.state = document.getElementById(this.state);
+		this.colnum = Math.floor(document.documentElement.clientWidth / this.width);
+		window.onload = function(){
+			_this.load();
+		};
+		window.onresize = function(){
+			_this.throttle(function(){
+				var width = this.width;
+				var colnum = Math.floor(document.documentElement.clientWidth / width);
+				var ncolnum = this.ncolnum;
+				var box = this.box;
+				colnum < ncolnum ? (colnum = ncolnum,box.style.width = colnum * width + "px" ) : (box.style.cssText = "");
+	
+				if(colnum !== this.colnum){
+					this.colnum = colnum;
+					var eles;
+					var clone;
+					for(var i = 0; i < this.colnum; i++){
+						this.cols[i] && (this.cols[i]['height'] = 0);
+					}
+					this.colnum = colnum;
+					this.removeFilled();
+					clone = this.box.cloneNode(true);
+					eles = this.children(clone);
+					this.box.innerHTML = "";
+					this.box.appendChild(clone);
+					this.cols = [];
+					this.maxH = 0;
+					this.length = 0;
+					this.arrange(eles,0);
+				}
+			},_this,200);
+		};
 
-		var imgs = temp.getElementsByTagName("img");
-		rsize ? arrange.call(this) : imgReady(imgs,arrange,this);
+		window.onscroll = function(){
+			_this.throttle(function(){
+				var sTop = document.documentElement.scrollTop || document.body.scrollTop;
+				if(document.body.clientHeight - document.documentElement.clientHeight - sTop < 50){
+					this.removeFilled();
+					this.load();
+				}
+			},_this,500);
+		}
+	};
 
-		function arrange(){
-			for(var i = old.length, len = eles.length; i < len; i++){
+	waterfall.init.prototype = {
+		children : function(elem){
+			var temp = elem.childNodes;
+			var len = temp.length;
+			var nodeList = [];
+			while(len--){
+				var o = temp[len];
+				o.nodeType === 1 && nodeList.unshift(o);
+			}
+			return nodeList;
+		},
+
+		throttle : function(fn,context,time){
+			time = time || 100;
+			clearTimeout(this.timer);
+			this.timer = setTimeout(function(){
+				fn.call(context);
+			},time);
+		},
+
+		imgReady : function(imgs,fn,context,args){
+			var count = imgs.length;
+			var len = count;
+			if(!count){
+				return fn.call(context);
+			}
+			for(var i = 0; i < len; i++){
+				var Img = imgs[i];
+				if(typeof Img.onreadystatechange !== "undefined"){
+					Img.onreadystatechange = function(){
+						this.readyState === "complete" && check();
+					}
+				}else{
+					Img.onload = function(){
+						this.onload = null;
+						check();
+					}
+				}
+			}
+			function check(){
+				count--;
+				!count && fn.apply(context,args);
+			}
+		},
+
+		appendFilled : function(){
+			var oFragment = document.createDocumentFragment();
+			var len = this.filled.length;
+			for(var i = 0; i < this.colnum - len; i++){
+				var oFilled = document.createElement("li");
+				oFilled.className = this.iclass;
+				oFilled.style.cssText = "height:0;overflow:hidden;visibility:hidden;padding-top:0;padding-bottom:0;";
+				if(document.querySelector){
+					var oNbsp = document.createTextNode("\n");
+					oFragment.appendChild(oNbsp);
+				}
+				oFragment.appendChild(oFilled);
+				this.filled.push(oFilled);
+			}
+			this.box.appendChild(oFragment);
+		},
+
+		removeFilled : function(){
+			var len = this.filled.length;
+			for(var i = 0; i < len; i++){
+				var oFilled = this.filled[i];
+				oFilled.parentNode.removeChild(oFilled);
+			}
+			this.filled = [];
+		},
+
+		arrange : function(eles,start){
+			var box = this.box;
+			var ncolnum = this.ncolnum;
+			var colnum;
+			var width = this.width;
+			var len = eles.length;
+			var oFragment = document.createDocumentFragment();
+			this.colnum < ncolnum ? (colnum = this.colnum = ncolnum,box.style.cssText = "width:" + colnum * width + "px") : (colnum = this.colnum,box.style.cssText = "");
+			for(var i = start; i < len; i++){
 				var o = eles[i];
-				var col = i % this.colnum;
+				var node = o.cloneNode(true);
+				var col = i % colnum;
 				o.h = o.offsetHeight;
-
 				if(i < this.colnum){
 					!this.cols[col] && (this.cols[col] = []);
 					!this.cols[col]["height"] && (this.cols[col]["height"] = 0);
 				}
-				if(i &&  i % this.colnum === 0){
-					for(var j = 0; j < this.colnum; j++){
-						this.maxH = Math.max(this.maxH,this.cols[j]["height"]);
+				if(i && i % colnum === 0){
+					for(var j = 0; j < colnum; j++){
+						this.maxH = Math.max(this.maxH,this.cols[j]['height']);
 					}
 				}
-				eles[i].style.marginTop = this.cols[col]["height"] - this.maxH + "px";
+				node.style.marginTop = this.cols[col]['height'] - this.maxH + "px";
 				this.cols[col]['height'] += o.h;
-				this.cols[col].push(o);
-			}
-			temp.outerHTML = temp.innerHTML;
-			temp = null;
-			this.appendFilled(this.context);
-		}
-	};
-
-	this.throttle = function(fn,context,time){
-		time = time || 100;
-		clearTimeout(this.timer);
-		this.timer = setTimeout(function(){
-			fn.call(context);
-		},time);
-	}
-	
-	window.onresize = function(){
-		_this.throttle(function(){
-			if(Math.floor(document.documentElement.clientWidth / this.w) != this.colnum){
-				for(var i = 0; i < this.colnum; i++){
-					this.cols[i]['height'] = 0;
+				oFragment.appendChild(node);
+				if(document.querySelector){
+					var oNbsp = document.createTextNode("\n");
+					oFragment.appendChild(oNbsp);
 				}
-				this.removeFilled();
-				var data = this.context.innerHTML;
-				this.colnum = Math.floor(document.documentElement.clientWidth / this.w);
-				this.cols = [];
-				this.maxH = 0;
-				this.context.innerHTML = "";
-				this.load(data,true);
+				this.cols.push(o);
 			}
-		},_this,100);
+			this.length = eles.length;
+			//alert(eles)
+			eles[start].parentNode !== this.box && this.box.removeChild(eles[start].parentNode);
+			this.box.appendChild(oFragment);
+			this.appendFilled();
+		},
+
+		load : function(){
+			var _this = this;
+			var url = this.url + "?args=" + this.count;
+			var request = ajax({
+				"url" : url,
+				"success" : function(data){
+					if(data.length === 0){
+						_this.state.innerHTML = "没有更多数据了";
+					}else{
+						var eles = Array(_this.length);
+						var temp = document.createElement("ul");
+						var imgs;
+						temp.style.cssText = "visibility:hidden";
+						temp.innerHTML = data;
+						_this.box.appendChild(temp);
+						imgs = temp.getElementsByTagName("img");
+						eles = eles.concat(_this.children(temp));
+						_this.imgReady(imgs,_this.arrange,_this,[eles,_this.length]);
+					}
+				},
+				"error" : function(msg){
+					_this.state.innerHTML = msg;
+				}
+			});
+			this.count++;
+		}
 	};
 
-	window.onscroll = function(){
-		var sTop = document.documentElement.scrollTop || document.body.scrollTop;
-		if(document.body.clientHeight - document.documentElement.clientHeight - sTop < 10){
-			_this.throttle(function(){
-				_this.removeFilled();
-				_this.load(data,false);
-			},_this,200);
-		}
-	}
+	var w = waterfall({
+		"id"      : "waterfall",			//容器id
+		"state"   : "loading",				//提示信息id
+		"ncolnum" : 3,						//默认列数
+		"width"   : 218,					//列表项宽度
+		'iclass'  : "justify_item",			//列表项类名
+		'url'	  : "waterfall.php",		//请求url
+		"count"	  : 0						//load计数器，用用于生成新请求的url
+	});
+}());
 
-	this.load(data,false);
-};
-
-var time = +new Date();
-var w = new waterfall({
-	"id"            : "waterfall",
-	"w"             : 218,
-	"itemClassName" : "justify_item"
-});
-//document.title = (+new Date() - time);
