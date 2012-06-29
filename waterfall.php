7512eca7801146511b7a735b3ce7ef9ab0d126b4
cage <517020375@qq.com>
2012-06-29 13:50:58 +0800

    ..

---
+++ waterfall.php
@@ -0,0 +1,76 @@
+<?php
+
+	$random_number = mt_rand(1,3);
+	
+	switch($random_number){
+	case 1:
+		echo  '<li class="justify_item"><a href="#"><img src="img/1.jpg" alt="" ><span>01</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/2.jpg" alt="" ><span>02</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/3.jpg" alt="" ><span>03</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/4.jpg" alt="" ><span>04</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/5.jpg" alt="" ><span>05</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/6.jpg" alt="" ><span>06</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/7.jpg" alt="" ><span>07</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/8.jpg" alt="" ><span>08</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/9.jpg" alt="" ><span>09</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/10.jpg" alt="" ><span>10</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/11.jpg" alt="" ><span>11</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/12.jpg" alt="" ><span>12</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/13.jpg" alt="" ><span>13</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/14.jpg" alt="" ><span>14</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/15.jpg" alt="" ><span>15</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/16.jpg" alt="" ><span>16</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/17.jpg" alt="" ><span>17</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/18.jpg" alt="" ><span>18</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/19.jpg" alt="" ><span>19</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/20.jpg" alt="" ><span>20</span></a></li>';
+		break;
+	case 2:
+		echo  '<li class="justify_item"><a href="#"><img src="img/21.jpg" alt="" ><span>21</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/22.jpg" alt="" ><span>22</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/23.jpg" alt="" ><span>23</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/24.jpg" alt="" ><span>24</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/25.jpg" alt="" ><span>25</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/26.jpg" alt="" ><span>26</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/27.jpg" alt="" ><span>27</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/28.jpg" alt="" ><span>28</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/29.jpg" alt="" ><span>29</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/30.jpg" alt="" ><span>30</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/31.jpg" alt="" ><span>31</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/32.jpg" alt="" ><span>32</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/33.jpg" alt="" ><span>33</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/34.jpg" alt="" ><span>34</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/35.jpg" alt="" ><span>35</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/36.jpg" alt="" ><span>36</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/37.jpg" alt="" ><span>37</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/38.jpg" alt="" ><span>38</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/39.jpg" alt="" ><span>39</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/40.jpg" alt="" ><span>40</span></a></li>';
+		break;
+	case 3:
+		echo  '<li class="justify_item"><a href="#"><img src="img/41.jpg" alt="" ><span>41</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/42.jpg" alt="" ><span>42</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/43.jpg" alt="" ><span>43</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/44.jpg" alt="" ><span>44</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/45.jpg" alt="" ><span>45</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/46.jpg" alt="" ><span>46</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/47.jpg" alt="" ><span>47</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/48.jpg" alt="" ><span>48</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/49.jpg" alt="" ><span>49</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/50.jpg" alt="" ><span>50</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/51.jpg" alt="" ><span>51</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/52.jpg" alt="" ><span>52</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/53.jpg" alt="" ><span>53</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/54.jpg" alt="" ><span>54</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/55.jpg" alt="" ><span>55</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/56.jpg" alt="" ><span>56</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/57.jpg" alt="" ><span>57</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/58.jpg" alt="" ><span>58</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/59.jpg" alt="" ><span>59</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/60.jpg" alt="" ><span>60</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/61.jpg" alt="" ><span>61</span></a></li>
+			   <li class="justify_item"><a href="#"><img src="img/62.jpg" alt="" ><span>62</span></a></li>';
+		break;
+	}
+
+?>
